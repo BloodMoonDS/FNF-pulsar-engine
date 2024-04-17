21241b4b1ba8b468f09cf5575f3cc2135bf6859a
Bloodiey <71611133+BloodMoonDS@users.noreply.github.com>
2024-04-16 19:44:18 -0600

    First Commit

---
+++ source/OutdatedState.hx
@@ -0,0 +1,62 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+
+class OutdatedState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var warnText:FlxText;
+	override function create()
+	{
+		super.create();
+
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		warnText = new FlxText(0, 0, FlxG.width,
+			"You seem to use an \n
+			early version of vs bloodiey mod current version is: (" + MainMenuState.psychEngineVersion + "),\n
+			please update to " + TitleState.updateVersion + " When aviable!\n
+			Press ESCAPE to proceed anyway.\n
+			\n
+			Thank you for testing our mod!",
+			32);
+		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		warnText.screenCenter(Y);
+		add(warnText);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if(!leftState) {
+			if (controls.ACCEPT) {
+				leftState = true;
+				CoolUtil.browserLoad("https://github.com/BloodMoonDS/vs-bloodiey-fnf/releases");
+			}
+			else if(controls.BACK) {
+				leftState = true;
+			}
+
+			if(leftState)
+			{
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				FlxTween.tween(warnText, {alpha: 0}, 1, {
+					onComplete: function (twn:FlxTween) {
+						MusicBeatState.switchState(new MainMenuState());
+					}
+				});
+			}
+		}
+		super.update(elapsed);
+	}
+}
